591b160b7354ef3778f6c7d51ead16b145243136
Alex <alexandra.grigorchenko@gmail.com>
2024-05-03 12:15:43 +0200

    init commit

---
+++ src/main/java/org/telran/codecrustpizza/CodeCrustPizzaApplication.java
@@ -0,0 +1,13 @@
+package org.telran.codecrustpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCrustPizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeCrustPizzaApplication.class, args);
+    }
+
+}
