b0af2fe6c8e977a9767943b88e7e4753b7e4ea63
Alex <alexandra.grigorchenko@gmail.com>
2024-05-03 14:12:44 +0200

    add connection between User and Address

--- src/main/java/org/telran/codecrustpizza/entity/Address.java
+++ src/main/java/org/telran/codecrustpizza/entity/Address.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,6 +12,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import java.util.Set;
 
 @Data
 @Entity
@@ -27,4 +30,7 @@ public class Address {
     private String street;
     private String house;
     private String comment;
+
+    @ManyToMany(mappedBy = "addressSet")
+    private Set<User> users;
 }
--- src/main/java/org/telran/codecrustpizza/entity/User.java
+++ src/main/java/org/telran/codecrustpizza/entity/User.java
@@ -1,11 +1,15 @@
 package org.telran.codecrustpizza.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -26,7 +30,7 @@ import java.util.Set;
 @NoArgsConstructor
 @EqualsAndHashCode(exclude = {"addressSet", "favoritePizzaSet"})
 @ToString(exclude = {"addressSet", "favoritePizzaSet"})
-@Table(name = "user_")
+@Table(name = "_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,10 +39,30 @@ public class User {
     private String email;
     private String password;
     private String phone;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_address",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id"))
     private Set<Address> addressSet;
+
     @Enumerated(value = EnumType.STRING)
     private Role role;
     private boolean isBlocked;
+
+
     private Set<Pizza> favoritePizzaSet;
+
     private LocalDateTime creationDate;
+
+
+    public void addAddress(Address address) {
+        addressSet.add(address);
+        address.getUsers().add(this);
+    }
+
+    public void removeAddress(Address address) {
+        addressSet.remove(address);
+        address.getUsers().remove(this);
+    }
 }
