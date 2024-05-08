536c402a7f9b31c0e4e43a71932acad92d17ed75
Alex <alexandra.grigorchenko@gmail.com>
2024-05-08 12:23:54 +0200

    add EntityException and messages for this exception

---
+++ src/main/java/org/telran/codecrustpizza/exception/ExceptionMessage.java
@@ -0,0 +1,36 @@
+package org.telran.codecrustpizza.exception;
+
+public enum ExceptionMessage {
+
+    NO_SUCH_ID("There is no %s with id: %d"),
+    NO_SUCH_EMAIL("There is no %s with email: %s"),
+    EMAIL_EXIST("%s with such email: %s already exist"),
+    ENTITY_NOT_EXIST("This %s doesn't exist"),
+    ENTITY_EXIST("This %s already exist");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCustomMessage() {
+        return message;
+    }
+
+    public String getCustomMessage(Long id) {
+        return String.format(message, id);
+    }
+
+    public String getCustomMessage(String entity) {
+        return String.format(message, entity);
+    }
+
+    public String getCustomMessage(String entity, String param) {
+        return String.format(message, entity, param);
+    }
+
+    public String getCustomMessage(String entity, Long id) {
+        return String.format(message, entity, id);
+    }
+}
