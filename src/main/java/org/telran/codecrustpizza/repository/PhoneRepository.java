8e4ccda7e07cafb44460bd55673c60e2a546a160
Alex <alexandra.grigorchenko@gmail.com>
2024-05-08 12:25:39 +0200

    add to Phone and Address repositories methods for search equal entities

--- src/main/java/org/telran/codecrustpizza/repository/PhoneRepository.java
+++ src/main/java/org/telran/codecrustpizza/repository/PhoneRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.telran.codecrustpizza.entity.Phone;
 
+import java.util.Optional;
+
 @Repository
 public interface PhoneRepository extends JpaRepository<Phone, Long> {
 
+    Optional<Phone> findByCountryCodeAndNumber(String countryCode, String number);
 }
