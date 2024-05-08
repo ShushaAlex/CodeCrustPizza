d97630bc27dedc45b53991f65229c3a2ef171389
Alex <alexandra.grigorchenko@gmail.com>
2024-05-08 15:20:35 +0200

    add tests for user service

---
+++ src/test/java/org/telran/codecrustpizza/TestData.java
@@ -0,0 +1,83 @@
+package org.telran.codecrustpizza;
+
+import org.telran.codecrustpizza.dto.address.AddressCreateRequestDto;
+import org.telran.codecrustpizza.dto.phone.PhoneCreateRequestDto;
+import org.telran.codecrustpizza.dto.user.UserCreateRequestDto;
+import org.telran.codecrustpizza.dto.user.UserResponseDto;
+import org.telran.codecrustpizza.entity.Phone;
+import org.telran.codecrustpizza.entity.User;
+
+import java.time.LocalDateTime;
+
+public class TestData {
+    // 1 set of user data
+    public static LocalDateTime LOCAL_DATE_TIME_1 = LocalDateTime.of(2023, 1, 1, 1, 1);
+    public static String EMAIL_1 = "john.doe@example.com";
+    public static String NAME_1 = "John Doe";
+    public static String PASSWORD_1 = "1234";
+    // 2 set of user data
+    public static LocalDateTime LOCAL_DATE_TIME_2 = LocalDateTime.of(2022, 5, 5, 12, 30);
+    public static String EMAIL_2 = "jane.smith@example.com";
+    public static String NAME_2 = "Jane Smith";
+    public static String PASSWORD_2 = "4321";
+    // 1 set of phone data
+    public static String TITLE_1 = "mobile";
+    public static String COUNTRY_CODE_1 = "+1";
+    public static String AREA_CODE_1 = "123";
+    public static String NUMBER_1 = "456789";
+    public static String EXTENSION_1 = "12";
+    // 1 set of address data
+    public static String CITY_1 = "city1";
+    public static String STREET_1 = "street1";
+    public static String HOUSE_1 = "house1";
+    public static String COMMENT_1 = "comment1";
+
+    public static UserResponseDto USER_RESPONSE_DTO_1 = new UserResponseDto(
+            1L,
+            NAME_1,
+            EMAIL_1,
+            null,
+            null,
+            false,
+            LOCAL_DATE_TIME_1);
+
+    public static User USER_1 = User.builder()
+            .id(1L)
+            .name(NAME_1)
+            .email(EMAIL_1)
+            .password(PASSWORD_1)
+            .creationDate(LOCAL_DATE_TIME_1)
+            .build();
+
+    public static UserCreateRequestDto USER_CREATE_DTO_1 = new UserCreateRequestDto(
+            NAME_1,
+            EMAIL_1,
+            PASSWORD_1,
+            PASSWORD_1
+    );
+
+    public static PhoneCreateRequestDto PHONE_CREATE_DTO_1 = new PhoneCreateRequestDto(
+            TITLE_1,
+            COUNTRY_CODE_1,
+            AREA_CODE_1,
+            NUMBER_1,
+            EXTENSION_1
+    );
+
+    public static Phone PHONE_1 = Phone.builder()
+            .id(1L)
+            .title(TITLE_1)
+            .countryCode(COUNTRY_CODE_1)
+            .areaCode(AREA_CODE_1)
+            .number(NUMBER_1)
+            .extension(EXTENSION_1)
+            .build();
+
+    public static AddressCreateRequestDto ADDRESS_CREATE_DTO_1 = new AddressCreateRequestDto(
+            CITY_1,
+            STREET_1,
+            HOUSE_1,
+            COMMENT_1
+    );
+}
+
--- src/test/java/org/telran/codecrustpizza/service/impl/UserServiceImplTest.java
+++ src/test/java/org/telran/codecrustpizza/service/impl/UserServiceImplTest.java
@@ -6,8 +6,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ContextConfiguration;
+import org.telran.codecrustpizza.dto.phone.PhoneCreateRequestDto;
+import org.telran.codecrustpizza.dto.user.UserCreateRequestDto;
 import org.telran.codecrustpizza.dto.user.UserResponseDto;
+import org.telran.codecrustpizza.entity.Phone;
 import org.telran.codecrustpizza.entity.User;
+import org.telran.codecrustpizza.entity.enums.Role;
 import org.telran.codecrustpizza.mapper.AddressMapper;
 import org.telran.codecrustpizza.mapper.PhoneMapper;
 import org.telran.codecrustpizza.mapper.UserMapper;
@@ -23,8 +27,11 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.telran.codecrustpizza.service.impl.TestData.USER_1;
-import static org.telran.codecrustpizza.service.impl.TestData.USER_RESPONSE_DTO_1;
+import static org.telran.codecrustpizza.TestData.EMAIL_1;
+import static org.telran.codecrustpizza.TestData.PHONE_CREATE_DTO_1;
+import static org.telran.codecrustpizza.TestData.USER_1;
+import static org.telran.codecrustpizza.TestData.USER_CREATE_DTO_1;
+import static org.telran.codecrustpizza.TestData.USER_RESPONSE_DTO_1;
 
 @ExtendWith(MockitoExtension.class)
 @ContextConfiguration(classes = UserServiceImpl.class) // to specify which class we're testing
@@ -47,7 +54,7 @@ public class UserServiceImplTest {
     private UserServiceImpl userServiceImpl;
 
     @Test
-    public void findAllUsers() {
+    public void findAllUsersTest() {
         // Prepare data
         List<User> users = List.of(new User(), new User(), new User());
         when(userRepository.findAll()).thenReturn(users);
@@ -60,7 +67,7 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void findUserById() {
+    public void findUserByIdTest() {
         // Prepare data
         User user = USER_1;
         Long userId = 1L;
@@ -71,17 +78,116 @@ public class UserServiceImplTest {
         // Validate
         assertEquals(result.name(), USER_RESPONSE_DTO_1.name());
         assertEquals(result.email(), USER_RESPONSE_DTO_1.email());
+        verify(userRepository, times(1)).findById(userId);
+        verify(userMapper, times(1)).toResponseDto(user);
     }
 
     @Test
-    public void findUserByEmail() {
-        // similar to the findUserById test
+    public void findUserByEmailTest() {
+        // Prepare data
+        User user = USER_1;
+        String email = EMAIL_1;
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        when(userMapper.toResponseDto(user)).thenReturn(USER_RESPONSE_DTO_1);
+        // Execute
+        UserResponseDto result = userServiceImpl.findByEmail(email);
+        // Validate
+        assertEquals(result.name(), USER_RESPONSE_DTO_1.name());
+        assertEquals(result.email(), USER_RESPONSE_DTO_1.email());
+        verify(userRepository, times(1)).findByEmail(email);
+        verify(userMapper, times(1)).toResponseDto(user);
     }
 
     @Test
-    public void saveUser() {
-        // Please follow similar logic with your dto - save - validate flow
+    public void saveUserTest() {
+        // Prepare data
+        User user = USER_1;
+        String email = EMAIL_1;
+        UserCreateRequestDto createRequestDto = USER_CREATE_DTO_1;
+        when(userRepository.findByEmail(email)).thenReturn(Optional.empty());
+        when(userMapper.toUser(createRequestDto)).thenReturn(user);
+        when(userRepository.save(user)).thenReturn(user);
+        when(userMapper.toResponseDto(user)).thenReturn(USER_RESPONSE_DTO_1);
+        // Execute
+        UserResponseDto result = userServiceImpl.save(createRequestDto);
+        // Validate
+        assertEquals(result.name(), USER_RESPONSE_DTO_1.name());
+        assertEquals(result.email(), USER_RESPONSE_DTO_1.email());
+        verify(userRepository, times(1)).save(user);
+        verify(userRepository, times(1)).findByEmail(email);
+        verify(userMapper, times(1)).toUser(createRequestDto);
+        verify(userMapper, times(1)).toResponseDto(user);
     }
+
+    @Test
+    void assignRoleTest() {
+        // Prepare data
+        Long userId = 1L;
+        Role role = Role.ADMIN;
+        User user = new User();
+        user.setRole(Role.USER);
+        UserResponseDto responseDto = USER_RESPONSE_DTO_1;
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(userMapper.toResponseDto(user)).thenReturn(responseDto);
+        // Execute
+        UserResponseDto result = userServiceImpl.assignRole(userId, role);
+        // Validate
+        assertEquals(responseDto, result);
+        assertEquals(role, user.getRole());
+        verify(userRepository, times(1)).findById(userId);
+        verify(userRepository, times(1)).save(user);
+        verify(userMapper, times(1)).toResponseDto(user);
+    }
+
+    @Test
+    void addPhone() { //TODO добелать
+        // Prepare data
+        Long userId = 1L;
+        PhoneCreateRequestDto phoneDto = PHONE_CREATE_DTO_1;
+        User user = new User();
+        user.setId(userId);
+        Phone phone = new Phone();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(phoneMapper.toPhone(phoneDto)).thenReturn(phone);
+        when(phoneRepository.save(phone)).thenReturn(phone);
+        when(userMapper.toResponseDto(user)).thenReturn(USER_RESPONSE_DTO_1);
+        // Execute
+        UserResponseDto result = userServiceImpl.addPhone(userId, phoneDto);
+        // Validate
+        assertEquals(result.name(), USER_RESPONSE_DTO_1.name());
+        assertEquals(result.email(), USER_RESPONSE_DTO_1.email());
+        verify(userRepository, times(1)).findById(userId);
+        verify(phoneMapper, times(1)).toPhone(phoneDto);
+        verify(phoneRepository, times(1)).save(phone);
+        verify(userMapper, times(1)).toResponseDto(user);
+    }
+
+    @Test
+    void removePhone() {
+    }
+
+    @Test
+    void addAddress() {
+    }
+
+    @Test
+    void removeAddress() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void changeEmail() {
+    }
+
+    @Test
+    void changeName() {
+    }
+
 
     // other methods follow...
 }
