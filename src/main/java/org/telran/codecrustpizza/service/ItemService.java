da089b810e28f86b7bae8ba50828848f42b6874c
Alex <alexandra.grigorchenko@gmail.com>
2024-05-16 18:58:09 +0200

    add interface for Item service

---
+++ src/main/java/org/telran/codecrustpizza/service/ItemService.java
@@ -0,0 +1,25 @@
+package org.telran.codecrustpizza.service;
+
+import org.telran.codecrustpizza.dto.item.ItemCreateRequestDto;
+import org.telran.codecrustpizza.dto.item.ItemResponseDto;
+
+import java.util.List;
+
+public interface ItemService {
+
+    List<ItemResponseDto> getAll();
+
+    ItemResponseDto findById(Long id);
+
+    List<ItemResponseDto> findAllByCategory(String category);
+
+    ItemResponseDto saveItem(ItemCreateRequestDto createDto);
+
+    ItemResponseDto updateItem(Long id, ItemCreateRequestDto createRequestDto);
+
+    ItemResponseDto addCategory(Long id, String category);
+
+    ItemResponseDto removeCategory(Long id, String category);
+
+    ItemResponseDto deleteItem(Long id);
+}
