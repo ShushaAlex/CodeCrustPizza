dbfff11180aa3e4fac44fc50e9649ce86e2a0823
Alex <alexandra.grigorchenko@gmail.com>
2024-05-22 22:07:55 +0200

    add PizzaController with methods for creating PizzaPatterns

---
+++ src/main/java/org/telran/codecrustpizza/controller/IngredientController.java
@@ -0,0 +1,48 @@
+package org.telran.codecrustpizza.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.telran.codecrustpizza.dto.ingredient.IngredientCreateRequestDto;
+import org.telran.codecrustpizza.dto.ingredient.IngredientResponseDto;
+import org.telran.codecrustpizza.service.IngredientService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/ingredient")
+@RequiredArgsConstructor
+public class IngredientController {
+
+    private final IngredientService ingredientService;
+
+    @GetMapping
+    List<IngredientResponseDto> getAll() {
+
+        return ingredientService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    IngredientResponseDto getById(@PathVariable Long id) {
+
+        return ingredientService.getById(id);
+    }
+
+    @PostMapping
+    IngredientResponseDto saveIngredient(@Valid @RequestBody IngredientCreateRequestDto createDto) {
+
+        return ingredientService.saveIngredient(createDto);
+    }
+
+    @PutMapping("/{id}/update")
+    IngredientResponseDto updateIngredient(@PathVariable Long id, @Valid @RequestBody IngredientCreateRequestDto createDto) {
+
+        return ingredientService.updateIngredient(id, createDto);
+    }
+}
---
+++ src/main/java/org/telran/codecrustpizza/controller/PizzaController.java
@@ -0,0 +1,53 @@
+package org.telran.codecrustpizza.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.telran.codecrustpizza.dto.pizza.PizzaCreateRequestDto;
+import org.telran.codecrustpizza.dto.pizza.PizzaResponseDto;
+import org.telran.codecrustpizza.dto.pizza.pizzaPattern.PizzaPatternCreateDto;
+import org.telran.codecrustpizza.dto.pizza.pizzaPattern.PizzaPatternResponseDto;
+import org.telran.codecrustpizza.entity.PizzaIngredient;
+import org.telran.codecrustpizza.entity.PizzaPatternIngredient;
+import org.telran.codecrustpizza.service.PizzaService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/pizza")
+@RequiredArgsConstructor
+public class PizzaController {
+
+    private final PizzaService<PizzaResponseDto, PizzaCreateRequestDto, PizzaIngredient> pizzaService;
+    private final PizzaService<PizzaPatternResponseDto, PizzaPatternCreateDto, PizzaPatternIngredient> pizzaPatternService;
+
+    @GetMapping
+    public List<PizzaResponseDto> findAllPizza() {
+
+        return pizzaService.findAll();
+    }
+
+    @GetMapping("/pattern")
+    public List<PizzaPatternResponseDto> findAllPattern() {
+
+        return pizzaPatternService.findAll();
+    }
+
+    @PostMapping
+    public PizzaResponseDto createPizza(@Valid @RequestBody PizzaCreateRequestDto createDto) {
+
+        return pizzaService.createPizza(createDto);
+    }
+
+    @PostMapping("/pattern")
+    public PizzaPatternResponseDto createPizza(@Valid @RequestBody PizzaPatternCreateDto createDto) {
+
+        return pizzaPatternService.createPizza(createDto);
+    }
+
+
+}
