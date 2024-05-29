d81e58ecb0130cb101349d6e03bbccded63d7d70
Alex <alexandra.grigorchenko@gmail.com>
2024-05-29 11:52:14 +0200

    fix bug in item repository with join fetch of categories to item

--- src/main/java/org/telran/codecrustpizza/repository/ItemRepository.java
+++ src/main/java/org/telran/codecrustpizza/repository/ItemRepository.java
@@ -11,13 +11,12 @@ import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
-    @Query("SELECT i FROM Item i JOIN FETCH i.categories WHERE i.title = :title")
+    @Query("SELECT i FROM Item i LEFT JOIN FETCH i.categories WHERE i.title = :title")
     Optional<Item> findByTitle(String title);
 
-    @Query("SELECT i FROM Item i JOIN FETCH i.categories WHERE i.id = :id")
+    @Query("SELECT i FROM Item i LEFT JOIN FETCH i.categories WHERE i.id = :id")
     Optional<Item> findById(Long id);
 
-    @Query("SELECT i FROM Item i JOIN FETCH i.categories c WHERE c.title = :category")
+    @Query("SELECT i FROM Item i LEFT JOIN FETCH i.categories c WHERE c.title = :category")
     List<Item> findByCategory(String category);
-
 }
