52cdb08159d80bcf6ce82725aca65d0f8c813729
Alex <alexandra.grigorchenko@gmail.com>
2024-06-01 15:58:22 +0200

    add controllerAdvise

---
+++ src/main/java/org/telran/codecrustpizza/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,31 @@
+package org.telran.codecrustpizza.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {EntityException.class})
+    @ResponseBody
+    protected ResponseEntity<Object> handleConflict(
+            RuntimeException ex, WebRequest request) {
+        String bodyOfResponse = ex.getMessage();
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+
+    @ExceptionHandler(value = {AccessDeniedException.class})
+    @ResponseBody
+    public ResponseEntity<Object> handleAccessDeniedException(Exception ex, WebRequest request) {
+        return new ResponseEntity<Object>(
+                "You need authentication for this request", new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+}
