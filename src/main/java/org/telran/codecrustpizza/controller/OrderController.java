a14501c946532c1bb651fc361da9ffab50b9e9ad
Alex <alexandra.grigorchenko@gmail.com>
2024-06-09 17:55:07 +0200

    add swagger annotations for OrderController

--- src/main/java/org/telran/codecrustpizza/controller/OrderController.java
+++ src/main/java/org/telran/codecrustpizza/controller/OrderController.java
@@ -1,5 +1,9 @@
 package org.telran.codecrustpizza.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +23,16 @@ import java.util.List;
 @RestController
 @RequestMapping("api/order")
 @RequiredArgsConstructor
+@Tag(name = "Order Controller", description = "Operations related to order management")
 public class OrderController {
 
     private final UserService userService;
     private final OrderService orderService;
 
+    @Operation(summary = "Get current user's orders", description = "Retrieve a list of orders for the current user.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of orders")
+    })
     @GetMapping
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     public List<OrderResponseDto> getCurrentUserOrders() {
@@ -32,6 +41,12 @@ public class OrderController {
         return orderService.getOrdersByUserId(userId);
     }
 
+    @Operation(summary = "Get order by ID", description = "Retrieve an order by its ID. Requires admin privileges.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved order"),
+            @ApiResponse(responseCode = "400", description = "Order not found"),
+            @ApiResponse(responseCode = "403", description = "Access denied")
+    })
     @GetMapping("/{orderId}")
     @PreAuthorize("hasAuthority('ADMIN')")
     OrderResponseDto getOrderById(@PathVariable Long orderId) {
@@ -39,6 +54,11 @@ public class OrderController {
         return orderService.getOrderById(orderId);
     }
 
+    @Operation(summary = "Create an order", description = "Create a new order for the current user. Order creates from cart items.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully created order"),
+            @ApiResponse(responseCode = "400", description = "Cart is empty")
+    })
     @PostMapping("/create")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     OrderResponseDto createOrder(@RequestParam Long addressId) {
@@ -47,6 +67,12 @@ public class OrderController {
         return orderService.createOrder(userId, addressId);
     }
 
+    @Operation(summary = "Cancel an order", description = "Cancel an order by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully canceled order"),
+            @ApiResponse(responseCode = "400", description = "Order not found"),
+            @ApiResponse(responseCode = "400", description = "Order can not be canceled")
+    })
     @PutMapping("/{orderId}/cancel")
     @PreAuthorize("hasAnyAuthority('ADMIN', 'USER')")
     OrderResponseDto cancelOrder(@PathVariable Long orderId) {
@@ -54,6 +80,12 @@ public class OrderController {
         return orderService.cancelOrder(orderId);
     }
 
+    @Operation(summary = "Update order status", description = "Update the status of an order. Requires admin privileges.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated order status"),
+            @ApiResponse(responseCode = "400", description = "Order not found"),
+            @ApiResponse(responseCode = "403", description = "Access denied")
+    })
     @PutMapping("/{orderId}/updateStatus")
     @PreAuthorize("hasAuthority('ADMIN')")
     OrderResponseDto updateOrderStatus(@PathVariable Long orderId, @RequestParam OrderStatus status) {
